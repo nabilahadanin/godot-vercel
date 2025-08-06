c88387099cfd2526e1836321af7723ff818ffaf8
Nabilah Adani <nabilahadanin@gmail.com>
2025-08-06 14:59:06 +0700

    fix

--- api/fetch_player.js
+++ api/fetch_player.js
@@ -7,6 +7,10 @@ export default async function handler(req, res) {
   const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldXB4YXJka3JjZ29zZGZoemZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2OTk1NjMsImV4cCI6MjA2NzI3NTU2M30.sAv9ouFlirdNZ--M_NBYap51pcGGpXToW2JQ8DqmQVQ";
   const TOKEN = req.headers.authorization?.split(' ')[1];
   
+  console.log("🔥 Fetching player data...");
+  console.log("Request body:", req.body);
+  console.log("Request headers:", req.headers);
+  
   try {
     const { user_id } = req.body;
 
